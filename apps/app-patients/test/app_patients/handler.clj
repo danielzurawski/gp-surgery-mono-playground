993279d80776823169ccf977bd6d7e36932ae973
Daniel Żurawski <zurawski.daniel@gmail.com>
2020-03-28 12:34:16 +0000

    feat(): POST and GET endpoint tests

--- apps/app-patients/test/app_patients/handler.clj
+++ apps/app-patients/test/app_patients/handler.clj
@@ -1,7 +1,7 @@
 (ns patients.test.handler
   (:require [cheshire.core :as cheshire]
             [clojure.test :refer :all]
-            [patients.handler :refer :all]
+            [patients.handler :as handler]
             [ring.mock.request :as mock]))
 
 (defn parse-body [body]
@@ -10,15 +10,38 @@
 (def patient-record
   {:name "Daniel Zurawski"
    :description "nothing"
-   :gender "M"
+   :gender :M
    :origin {:country "Poland"
             :city "Bydgoszcz"}})
 
+(defn mock-get-all-records [& args]
+  [patient-record])
+
+(defn mock-save-patient-record! [& args]
+  patient-record)
+
+(defn mock-stream [& args])
+
+(defn stringify-gender [record]
+  (update-in record [:gender] name))
+
+(defn expected-patient-records []
+  (map stringify-gender (mock-get-all-records)))
+
 (deftest patients-test
-  (testing "Test POST request to /patients/echo, returns patient record"
-    (let [response (app (-> (mock/request :post "/patients/echo")
-                            (mock/json-body patient-record)))
-          body     (parse-body (:body response))]
+  (testing "Test POST request to /patients, return sent record"
+    (let [web-handler (handler/init mock-get-all-records mock-save-patient-record! mock-stream)
+          response    (web-handler (-> (mock/request :post "/patients")
+                                       (mock/content-type "application/json")
+                                       (mock/body (cheshire.core/generate-string patient-record))))
+          body        (parse-body (:body response))]
+      (is (= (:status response) 200))
+      (is (= (stringify-gender patient-record) body))))
+
+  (testing "Test GET request to /patients, returns patient record"
+    (let [web-handler (handler/init mock-get-all-records mock-save-patient-record! mock-stream)
+          response    (web-handler (mock/request :get "/patients"))
+          body        (parse-body (:body response))]
 
       (is (= 200 (:status response)))
-      (is (= patient-record body)))))
+      (is (= (expected-patient-records) body)))))
