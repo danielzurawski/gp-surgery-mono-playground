0dd345d6fd66537af29091d6491b2ad9d0a0771e
Daniel Żurawski <zurawski.daniel@gmail.com>
2020-04-20 15:53:46 +0100

    feat(): with-redefs-fn gateway handler tests refactor, use protocol

--- apps/app-api-gateway/src/gateway/handler.clj
+++ apps/app-api-gateway/src/gateway/handler.clj
@@ -1,22 +1,27 @@
 (ns gateway.handler
-  (:require [compojure.api.sweet :refer :all]
+  (:require [compojure.api.sweet :refer [api context POST]]
             [compojure.api.exception :as ex]
-            [ring.util.http-response :refer :all]
+            [ring.util.http-response :refer [ok]]
             [schema.core :as s]
             [buddy.sign.jwt :as jwt]
-            [gateway.user :as user]
+            [gateway.user-service :as user]
             [mount.core :refer [defstate]]))
 
 (s/defschema LoginResponse
-  {:id s/Num
-   :token s/Str
-   :username s/Str})
+  {:token s/Str
+   :email s/Str})
 
 (s/defschema LoginRequest
-  {:username s/Str
+  {:email s/Str
    :password s/Str})
 
+(s/defschema RegisterRequest
+  {:email s/Str
+   :password s/Str
+   :type (s/enum :patient :admin)})
+
 (def jwt-secret "so-secret")
+
 
 (defn create-web-handler []
   (api
@@ -33,15 +38,20 @@
    (context "/api" []
      :tags ["api"]
 
+     (POST "/register" []
+       :body [data RegisterRequest]
+       :summary "Saves the user record"
+       (let [user (user/save-user data)]
+         (ok {:email (:email user)})))
+
      (POST "/login" []
        :return LoginResponse
        :body [login LoginRequest]
-       :summary "Returns id, username and a signed JWT token"
+       :summary "Returns email and a signed JWT token"
        (let [user (user/get-user login)
              token (jwt/sign user jwt-secret)]
          (ok {:token token
-                       :id (:id user)
-                       :username (:username user)}))))))
+              :email (:email user)}))))))
 
 (defstate web-handler
   :start (create-web-handler))
--- apps/app-api-gateway/test/gateway/handler_test.clj
+++ apps/app-api-gateway/test/gateway/handler_test.clj
@@ -2,7 +2,7 @@
   (:require [cheshire.core :as cheshire]
             [clojure.test :refer :all]
             [gateway.handler :as handler]
-            ;; [gateway.user :as user]
+            [gateway.user-service :as user-service]
             [ring.mock.request :as mock]
             [mount.core :as mount]))
 
@@ -13,25 +13,36 @@
   {:id 123 :type "patient" :username "test123"})
 
 (defn start-states [f]
-  (-> (mount.core/only #{#'gateway.handler/web-handler
-                         #'gateway.user/get-user})
-      (mount.core/swap {#'gateway.user/get-user get-user})
+  (-> (mount.core/only #{#'gateway.handler/web-handler})
       mount/start)
   (f))
 
 (use-fixtures :once start-states)
 
+(defn mock-get-user [data] data)
+(defn mock-save-user [data] data)
+
 (deftest authentication-tests
   (testing "test 123"
     (is (= 1 1) "should be equal"))
 
-  (testing "Test POST request to /api/login returns a JWT token"
-    (let [response (handler/web-handler (-> (mock/request :post "/api/login")
-                                            (mock/content-type "application/json")
-                                            (mock/body (cheshire.core/generate-string {:username "test123" :password "test"}))))
-          body     (parse-body (:body response))]
+  (testing "Test POST request to /api/register returns OK"
+    (with-redefs-fn {#'gateway.user-service/save-user mock-save-user}
+      #(let [test-user {:email "zurawski.daniel@gmail.com" :password "test123" :type :patient}
+             response  (handler/web-handler (-> (mock/request :post "/api/register")
+                                                (mock/content-type "application/json")
+                                                (mock/body (cheshire.core/generate-string test-user))))
+             body      (parse-body (:body response))]
+         (is (= 200 (:status response)))
+         (is (= "zurawski.daniel@gmail.com" (:email body))))))
 
-      (is (= 200 (:status response)))
-      (is (not-empty (:token body))  "Should return token")
-      (is (= "test123" (:username body)) "Should return username")
-      (is (= 123 (:id body))) "Should return ID")))
+  (testing "Test POST request to /api/login returns a JWT token"
+    (with-redefs-fn {#'gateway.user-service/get-user mock-get-user}
+      #(let [patient-data {:email "zurawski.daniel@gmail.com" :password "test"}
+             response (handler/web-handler (-> (mock/request :post "/api/login")
+                                               (mock/content-type "application/json")
+                                               (mock/body (cheshire.core/generate-string patient-data))))
+             body     (parse-body (:body response))]
+         (is (= 200 (:status response)))
+         (is (not-empty (:token body))  "Should return token")
+         (is (= "zurawski.daniel@gmail.com" (:email body)) "Should return username")))))
